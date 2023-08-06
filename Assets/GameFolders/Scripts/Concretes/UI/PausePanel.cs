e41df30d7163942bad3c7195f631505220977782
Burak Sevincli <brksvncli@gmail.com>
2023-08-06 17:40:36 +0300

    Levels Completed

---
+++ Assets/GameFolders/Scripts/Concretes/UI/PausePanel.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+namespace GameFolders.Scripts.Concretes.UI
+{
+    public class PausePanel : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            Time.timeScale = 0f;
+        }
+
+        private void OnDisable()
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
