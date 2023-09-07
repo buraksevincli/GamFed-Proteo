fa6dafb0bea9141c931145c3e7f36bf76e01100f
AysenurTuran <120721647+AysenurTuran@users.noreply.github.com>
2023-09-07 16:15:27 +0300

    Campfire NewTrigger Fixed

--- Assets/GameFolders/Scripts/Concretes/Controllers/FireNewTriggerController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controllers/FireNewTriggerController.cs
@@ -5,11 +5,12 @@ namespace GameFolders.Scripts.Concretes.Controllers
 {
     public class FireNewTriggerController : MonoBehaviour
     {
+        [SerializeField][Range(2.6f,10)] private float energyDecreaseCoefficient = 4;
         private void OnTriggerStay2D(Collider2D col)
         {
             if (col.gameObject.CompareTag("Player"))
             {
-                DataManager.Instance.EventData.OnSpendEnergy?.Invoke(DataManager.Instance.GameData.EnergyDecreaseCoefficient * 4 * Time.deltaTime);
+                DataManager.Instance.EventData.OnSpendEnergy?.Invoke(DataManager.Instance.GameData.EnergyDecreaseCoefficient * energyDecreaseCoefficient * Time.deltaTime);
             }
         }
     }
